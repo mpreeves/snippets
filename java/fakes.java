e0e0ccdfdc72f7965fd50ebb5b99314e1d43bca3
Erick Fleming <efleming969@gmail.com>
2016-11-10 18:54:07 -0500

    change java fakes to use "sut" language

--- java/fakes.java
+++ java/fakes.java
@@ -14,20 +14,21 @@ public class MockingExample {
   public void should_build_a_greeting_based_on_age() {
     DataService ds = mock( DataService.class );
     when( ds.getMessageBasedOnAge( 45 ) ).thenReturn( "Whazzup" );
-    Greeting g = new Greeting( ds );
+    Greeting1 sut = new Greeting1( ds );
 
-    String message = g.build( "Joe", 45 );
+    String actual = sut.build( "Joe", 45 );
 
-    assertEquals( "Whazzup, Joe!", message );
+    assertEquals( "Whazzup, Joe!", actual );
   }
 
   @Test
   public void should_send_an_email_message() {
     EmailService es = mock( EmailService.class );
-    Greeting g = new Greeting( es );
+    Greeting2 sut = new Greeting2( es );
 
-    g.sayHello( "Joe" );
+    sut.sayHello( "Joe" );
 
     verify( es ).sendEmail( "Whazzup, Joe!" );
   }
 }
+
